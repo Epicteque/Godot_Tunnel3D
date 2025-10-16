38f2464a533fdfb6068d6d17ed3a9b931e898c84
Epicteque <24en2326@barton.ac.uk>
2025-10-16 15:35:36 +0100

    Clean up Line Class

--- plugin/Tunnel3DLineScript.cs
+++ plugin/Tunnel3DLineScript.cs
@@ -8,16 +8,16 @@ public partial class Tunnel3D : Node3D
 
         public static float DistanceLineToPoint(Line line, Vector3 point)
         {
-            Vector3 t = line._normal;
+            Vector3 t = line._direction;
             Vector3 l = line._origin;
 
-            float dotProdTCoef = t.X * t.X + t.Y * t.Y + t.Z * t.Z;
+            float dotProdTCoefficient = t.X * t.X + t.Y * t.Y + t.Z * t.Z;
 
-            float dotProdConstCoef = t.X * (point.X - l.X) + t.Y * (point.Y - l.Y) + t.Z * (point.Z - l.Z);
+            float dotProdConstantCoefficient = t.X * (point.X - l.X) + t.Y * (point.Y - l.Y) + t.Z * (point.Z - l.Z);
 
-            float t_UNIT = dotProdConstCoef / dotProdTCoef;
+            float tNormalised = dotProdConstantCoefficient / dotProdTCoefficient;
 
-            Vector3 nearestPointOnLine = l + (t * Math.Clamp(t_UNIT, 0, 1));
+            Vector3 nearestPointOnLine = l + (t * Math.Clamp(tNormalised, 0, 1));
 
             return DistancePointToPoint(nearestPointOnLine, point);
         }
@@ -25,11 +25,13 @@ public partial class Tunnel3D : Node3D
 
         public static float DistanceLineToLine(Line line1, Line line2)
         {
-            Vector3 t = line1._normal;
+            Vector3 t = line1._direction;
             Vector3 l1 = line1._origin;
 
-            Vector3 u = line2._normal;
+            Vector3 u = line2._direction;
             Vector3 l2 = line2._origin;
+
+            // Handle edge-cases where direction is zero
 
             if (t == Vector3.Zero && u == Vector3.Zero)
             {
@@ -44,19 +46,23 @@ public partial class Tunnel3D : Node3D
                 return DistanceLineToPoint(line1, l2);
             }
 
-            // TCoef_EQ1 + UCoef_EQ1 = Const_EQ1   aT + bU = c
-            // TCoef_EQ2 + UCoef_EQ2 = Const_EQ2   dT + eU = f
+            // Create Simultaneous Equation Coefficients
 
-            float TCoef_EQ1 = t.X * t.X + t.Y * t.Y + t.Z * t.Z;
-            float UCoef_EQ1 = -(t.X * u.X + t.Y * u.Y + t.Z * u.Z);
+            float TCoef_EQ1 = t.X * t.X + t.Y * t.Y + t.Z * t.Z; 
+            float UCoef_EQ1 = -(t.X * u.X + t.Y * u.Y + t.Z * u.Z); // Dot Product
 
-            float TCoef_EQ2 = -UCoef_EQ1; // u.X * t.X + u.Y * t.Y + u.Z + t.Z
+            float TCoef_EQ2 = -UCoef_EQ1; // equivalent to u.X * t.X + u.Y * t.Y + u.Z + t.Z
             float UCoef_EQ2 = -(u.X * u.X + u.Y * u.Y + u.Z * u.Z);
 
-            float Const_EQ1 = t.X * (l2.X - l1.X) + t.Y * (l2.Y - l1.Y) + t.Z * (l2.Z - l1.Z);
-            float Const_EQ2 = u.X * (l2.X - l1.X) + u.Y * (l2.Y - l1.Y) + u.Z * (l2.Z - l1.Z);
+            Vector3 originDifference = (l2 - l1);
+
+            float Const_EQ1 = t.X * originDifference.X + t.Y * originDifference.Y + t.Z * originDifference.Z;
+            float Const_EQ2 = u.X * originDifference.X + u.Y * originDifference.Y + u.Z * originDifference.Z;
 
             // Solve Simultaneously
+
+            // TCoef_EQ1 + UCoef_EQ1 = Const_EQ1   aT + bU = c
+            // TCoef_EQ2 + UCoef_EQ2 = Const_EQ2   dT + eU = f
 
             // Find T
 
@@ -69,8 +75,6 @@ public partial class Tunnel3D : Node3D
 
             // Find U
 
-            //float uValue = (TCoef_EQ1 * tValue - Const_EQ1) / -UCoef_EQ1;
-
             float EQ1_scalar = TCoef_EQ2 / TCoef_EQ1;
 
             float scaledUCoef_EQ1 = EQ1_scalar * UCoef_EQ1;
@@ -78,42 +82,34 @@ public partial class Tunnel3D : Node3D
 
             float uValue = (Const_EQ2 - scaledConst_EQ1) / (UCoef_EQ2 - scaledUCoef_EQ1);
 
-
             // Closest points on line are within the line segment bounds 
 
             bool tInRange = tValue > 0.0f && tValue < 1.0f;
             bool uInRange = uValue > 0.0f && uValue < 1.0f;
 
+            // NaN values indicate any value is possible (for an unbounded line) or the simultaneous equation failed
 
-            // NaN values indicate any value is possible (for an unbounded line) - The lines are coincident/parallel
-
-            int iters = 1;
-
-            float tValue2 = 1f;
-            float uValue2 = 1f;
+            bool equationFailed = false;
 
             if (float.IsNaN(tValue))
             {
                 tValue = 0f;
-                iters = 4;
+                equationFailed = true;
             }
 
             if (float.IsNaN(uValue))
             {
                 uValue = 0f;
-                iters = 4;
+                equationFailed = true;
             }
-
 
             float distanceValue = float.MaxValue;
 
-            float[] tValues = new float[2] { tValue, tValue2 };
-            float[] uValues = new float[2] { uValue, uValue2 };
 
-            for (int i = 0; i < iters; i++)
+            if (!equationFailed)
             {
-                Vector3 tPoint = l1 + (t * Math.Clamp(tValues[i / 2], 0.0f, 1.0f));
-                Vector3 uPoint = l2 + (u * Math.Clamp(uValues[i % 2], 0.0f, 1.0f));
+                Vector3 tPoint = l1 + (t * Math.Clamp(tValue, 0.0f, 1.0f));
+                Vector3 uPoint = l2 + (u * Math.Clamp(uValue, 0.0f, 1.0f));
 
                 distanceValue = Math.Min(distanceValue, DistancePointToPoint(tPoint, uPoint));
 
@@ -127,203 +123,49 @@ public partial class Tunnel3D : Node3D
                     distanceValue = Math.Min(distanceValue, DistanceLineToPoint(line2, tPoint));
                 }
             }
+            else
+            {
+                float[] tValues = new float[2] { tValue, 1f };
+                float[] uValues = new float[2] { uValue, 1f };
+
+                for (int i = 0; i < 4; i++) // Apply for every combination of values
+                {
+                    Vector3 tPoint = l1 + (t * Math.Clamp(tValues[i / 2], 0.0f, 1.0f));
+                    Vector3 uPoint = l2 + (u * Math.Clamp(uValues[i % 2], 0.0f, 1.0f));
+
+                    distanceValue = Math.Min(distanceValue, DistancePointToPoint(tPoint, uPoint));
+
+                    if (!uInRange)
+                    {
+                        distanceValue = Math.Min(distanceValue, DistanceLineToPoint(line1, uPoint));
+                    }
+
+                    if (!tInRange)
+                    {
+                        distanceValue = Math.Min(distanceValue, DistanceLineToPoint(line2, tPoint));
+                    }
+                }
+            }
 
             return distanceValue;
         }
 
-        public static Vector3[] PositionsLineToLine(Line line1, Line line2, float[] bounds) // Function Only Used in Prototype Demonstration
+        public static float DistancePointToPoint(Vector3 vector1, Vector3 vector2)
         {
-
-            Vector3 PositionLineToPoint(Line line, Vector3 point)
-            {
-                Vector3 t = line._normal;
-                Vector3 l = line._origin;
-
-
-                float dotProdTCoef = t.X * t.X + t.Y * t.Y + t.Z * t.Z;
-
-                float dotProdConstCoef = t.X * (point.X - l.X) + t.Y * (point.Y - l.Y) + t.Z * (point.Z - l.Z);
-
-
-                float t_UNIT = dotProdConstCoef / dotProdTCoef;
-
-                Vector3 nearestPointOnLine = l + (t * Mathf.Clamp(t_UNIT, 0, 1));
-
-
-                return nearestPointOnLine;
-            }
-
-
-            Vector3 t = line1._normal;
-            Vector3 l1 = line1._origin;
-
-            Vector3 u = line2._normal;
-            Vector3 l2 = line2._origin;
-
-            if (t == Vector3.Zero && u == Vector3.Zero)
-            {
-                return new Vector3[] { l1, l2 };
-            }
-            else if (t == Vector3.Zero)
-            {
-                return new Vector3[] { l1, PositionLineToPoint(line2, l1) };
-            }
-            else if (u == Vector3.Zero)
-            {
-                return new Vector3[] { l2, PositionLineToPoint(line1, l2) };
-            }
-
-
-
-            // TCoef_EQ1 + UCoef_EQ1 = Const_EQ1   aT + bU = c
-            // TCoef_EQ2 + UCoef_EQ2 = Const_EQ2   dT + eU = f
-
-            float TCoef_EQ1 = t.X * t.X + t.Y * t.Y + t.Z * t.Z;
-            float UCoef_EQ1 = -(t.X * u.X + t.Y * u.Y + t.Z * u.Z);
-
-            float TCoef_EQ2 = -UCoef_EQ1; // u.X * t.X + u.Y * t.Y + u.Z + t.Z
-            float UCoef_EQ2 = -(u.X * u.X + u.Y * u.Y + u.Z * u.Z);
-
-            float Const_EQ1 = t.X * (l2.X - l1.X) + t.Y * (l2.Y - l1.Y) + t.Z * (l2.Z - l1.Z);
-            float Const_EQ2 = u.X * (l2.X - l1.X) + u.Y * (l2.Y - l1.Y) + u.Z * (l2.Z - l1.Z);
-
-            // Solve Simultaneously
-
-            // Find T
-
-            float EQ2_scalar = UCoef_EQ1 / UCoef_EQ2;
-
-            float scaledTCoef_EQ2 = EQ2_scalar * TCoef_EQ2;
-            float scaledConst_EQ2 = EQ2_scalar * Const_EQ2;
-
-            float tValue = (Const_EQ1 - scaledConst_EQ2) / (TCoef_EQ1 - scaledTCoef_EQ2);
-
-            // Find U
-
-            //float uValue = (TCoef_EQ1 * tValue - Const_EQ1) / -UCoef_EQ1;
-
-            float EQ1_scalar = TCoef_EQ2 / TCoef_EQ1;
-
-            float scaledUCoef_EQ1 = EQ1_scalar * UCoef_EQ1;
-            float scaledConst_EQ1 = EQ1_scalar * Const_EQ1;
-
-            float uValue = (Const_EQ2 - scaledConst_EQ1) / (UCoef_EQ2 - scaledUCoef_EQ1);
-
-
-            // Closest points on line are within the line segment bounds 
-
-            bool tInRange = tValue > 0.0f && tValue < 1.0f;
-            bool uInRange = uValue > 0.0f && uValue < 1.0f;
-
-            // NaN values indicate any value is possible - The lines are coincident
-
-            int iters = 1;
-
-            float tValue2 = 1f;
-            float uValue2 = 1f;
-
-            if (float.IsNaN(tValue))
-            {
-                tValue = 0f;
-                iters = 4;
-            }
-
-            if (float.IsNaN(uValue))
-            {
-                uValue = 0f;
-                iters = 4;
-            }
-
-
-            float distanceValue = float.MaxValue;
-
-            Vector3 closestPoint1 = Vector3.Zero;
-            Vector3 closestPoint2 = Vector3.Zero;
-
-
-
-            //GD.Print(distanceValue);
-
-            float[] tValues = new float[2] { tValue, tValue2 };
-
-            float[] uValues = new float[2] { uValue, uValue2 };
-
-            for (int i = 0; i < iters; i++) // In cases where T and U are NaN, all bounding points must be checked, otherwise continues as normal.
-            {
-                Vector3 tPoint = l1 + (t * Mathf.Clamp(tValues[i / 2], 0.0f, 1.0f));
-                Vector3 uPoint = l2 + (u * Mathf.Clamp(uValues[i % 2], 0.0f, 1.0f));
-
-                if (i == 0) // sets variables during first iteration.
-                {
-                    closestPoint1 = tPoint;
-                    closestPoint2 = uPoint;
-                }
-
-
-                distanceValue = Math.Min(distanceValue, DistancePointToPoint(tPoint, uPoint));
-
-                if (!uInRange)
-                {
-
-                    Vector3 p = PositionLineToPoint(line1, uPoint);
-                    if (DistancePointToPoint(uPoint, p) < distanceValue)
-                    {
-                        closestPoint1 = p;
-
-                        closestPoint2 = uPoint;
-
-                        distanceValue = Math.Min(distanceValue, DistancePointToPoint(uPoint, p));
-                    }
-
-                }
-
-                if (!tInRange)
-                {
-
-                    Vector3 p = PositionLineToPoint(line2, tPoint);
-
-                    //GD.Print(p);
-
-                    if (DistancePointToPoint(tPoint, p) < distanceValue)
-                    {
-                        closestPoint1 = tPoint;
-                        closestPoint2 = p;
-
-                        distanceValue = Math.Min(distanceValue, DistancePointToPoint(tPoint, p));
-
-                    }
-
-
-                }
-
-            }
-
-            //GD.Print($"{closestPoint1} {closestPoint2}");
-
-
-            //GD.Print($"T{tInRange} U{uInRange} RDist: {distanceValue} PDist:{DistancePointToPoint(closestPoint1, closestPoint2)}");
-
-
-
-            return new Vector3[] { closestPoint1, closestPoint2 };
-        }
-
-        public static float DistancePointToPoint(Vector3 v1, Vector3 v2)
-        {
-            Vector3 diff = v2 - v1;
+            Vector3 diff = vector2 - vector1;
 
             return MathF.Sqrt(diff.X * diff.X + diff.Y * diff.Y + diff.Z * diff.Z);
         }
 
         private Vector3 _origin;
         private Vector3 _endpoint;
-        private Vector3 _normal;
+        private Vector3 _direction;
 
         public Line(Vector3 origin, Vector3 endpoint)
         {
             _origin = origin;
             _endpoint = endpoint;
-            _normal = endpoint - origin;
+            _direction = endpoint - origin;
         }
 
     }
